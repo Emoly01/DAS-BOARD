5cf34af469ff75a796e2347782de5d8bc3ee8946
Emoly01 <132718081+Emoly01@users.noreply.github.com>
2026-02-27 15:37:40 +0100

    Replace Firebase Analytics with Firestore
    Removed analytics import and initialization, added Firestore import and export.

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDNgGC-3qksHbOWsKcEh50_5ZE6wH3n8aQ",
   authDomain: "dnd-tools-1dd87.firebaseapp.com",
   projectId: "dnd-tools-1dd87",
   storageBucket: "dnd-tools-1dd87.firebasestorage.app",
   messagingSenderId: "866582352851",
-  appId: "1:866582352851:web:269ec8b40fc5764425d526",
-  measurementId: "G-NDMWX2DC07"
+  appId: "1:866582352851:web:269ec8b40fc5764425d526"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
